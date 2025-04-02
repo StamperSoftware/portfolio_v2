e79b6ca4b51b570719c9916f7ee9a101afbb79b8
StamperSoftware <StamperSoftware@gmail.com>
2025-04-02 13:23:48 -0400

    added icons

--- client/src/app/ui/footer/footer.component.ts
+++ client/src/app/ui/footer/footer.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { FaIconComponent } from "@fortawesome/angular-fontawesome";
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [
+    FaIconComponent
+  ],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
 
+  protected readonly faLinkedin = faLinkedin;
+  protected readonly faGithub = faGithub;
 }

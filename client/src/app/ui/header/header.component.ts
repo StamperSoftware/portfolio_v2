7a0c2682b0786c6f7c3777d61486bdc35f390dd3
StamperSoftware <StamperSoftware@gmail.com>
2025-05-22 19:05:43 -0400

    added pdf

--- client/src/app/ui/header/header.component.ts
+++ client/src/app/ui/header/header.component.ts
@@ -1,17 +1,19 @@
 import { Component } from '@angular/core';
 import { NgbDropdown, NgbDropdownItem, NgbDropdownMenu, NgbDropdownToggle } from "@ng-bootstrap/ng-bootstrap";
 import { RouterLink } from "@angular/router";
+import { NgOptimizedImage } from "@angular/common";
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [
-    NgbDropdown,
-    NgbDropdownToggle,
-    NgbDropdownMenu,
-    NgbDropdownItem,
-    RouterLink
-  ],
+    imports: [
+        NgbDropdown,
+        NgbDropdownToggle,
+        NgbDropdownMenu,
+        NgbDropdownItem,
+        RouterLink,
+        NgOptimizedImage
+    ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
